a371627ce6b25283cb6a6cbce0c9d4fb89641fd1
Carlos <tiagocdz29@hotmail.com>
2023-04-30 22:49:09 -0300

    Mudança das cores do site e novos banner, footer e layout

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,7 +8,7 @@ import skill7 from "../assets/img/mongodb.png";
 import skill8 from "../assets/img/ts2.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import colorSharp from "../assets/img/color-sharp.png"
+import colorSharp from "../assets/img/color-sharp3.png"
 
 export const Skills = () => {
   const responsive = {
